6f6285d7ba20d42034b3143a80c19e78511f8476
gayatrimahajan26 <gayatrimahajan913@gmail.com>
2026-06-04 22:41:24 +0530

    Added user registration API with password hashing

--- user-service/server.js
+++ user-service/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const User = require("./models/User");
@@ -14,7 +16,36 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log("❌ MongoDB Error:", err));
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "User registered successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
 // Home Route
 app.get("/", (req, res) => {
   res.send("User Service Running 🚀");
